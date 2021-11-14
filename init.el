e2692a8920d07b398257dead652003c3feab1a6d
Emmanouil Theofanis Chourdakis <emmanouil.chourdakis@nomono.co>
2021-11-14 15:59:41 +0100

    a

--- init.el
+++ init.el
@@ -26,6 +26,10 @@
 
 					; Theme
 (load-theme 'melancholy t)
+(setq org-src-fontify-natively t)
+
+; Other
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,3 +42,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
