8ec5d7fb3b9a01f88b4c24b72a18ca5a2dc24de5
Emmanouil Theofanis Chourdakis <emmanouil.chourdakis@nomono.co>
2021-11-15 06:53:25 +0100

    Add recenf-mode and undo-tree-mode

--- init.el
+++ init.el
@@ -28,14 +28,22 @@
 (load-theme 'melancholy t)
 (setq org-src-fontify-natively t)
 
-; Other
+					; Get an undo tree
+(undo-tree-mode)
+
+					; Recentf-mode
+(recentf-mode)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit melancholy-theme plantuml-mode undo-tree)))
+ '(package-selected-packages
+   '(magit melancholy-theme plantuml-mode undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
