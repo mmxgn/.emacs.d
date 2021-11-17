ba0489dd7f798e656aa6e27db1678dfdcec8745c
Emmanouil Theofanis Chourdakis <emmanouil.chourdakis@nomono.co>
2021-11-17 10:34:09 +0100

    Added a bunch of interesting options

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 					; list of packages to be installed
-(setq package-list '(undo-tree plantuml-mode melancholy-theme magit))
+(setq package-list '(undo-tree plantuml-mode melancholy-theme magit markdown-mode))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -31,19 +31,31 @@
 					; Get an undo tree
 (undo-tree-mode)
 
-					; Recentf-mode
+					; Recentf-mode - Show recently used files using C-x r
 (recentf-mode)
 (setq recentf-max-menu-items 25)
 (setq recentf-max-saved-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
+					; Global visual line mode - Wrap words
+(global-visual-line-mode 1)
+					; Highlight parentheses and other characters
+(show-paren-mode 1)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((python . t) (plantuml . t))
+ )
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'ox-confluence)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends '(ascii html icalendar latex md odt confluence))
  '(package-selected-packages
-   '(magit melancholy-theme plantuml-mode undo-tree)))
+   '(markdown-mode magit melancholy-theme plantuml-mode undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
